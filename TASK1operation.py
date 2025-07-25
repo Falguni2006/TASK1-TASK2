1f0ebcf3967886d3e3d92622d8c4668abba80490
Falguni2006 <khadgipayal50@gmail.com>
2025-07-25 21:37:51 +0530

    Add files via upload

---
+++ TASK1operation.py
@@ -0,0 +1,11 @@
+num1 = float(input("Entre the first number: "))
+num2 = float(input("Entre the Second number: "))
+
+print("\nAddition:",num1+num2)
+print("subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+
+if num2 != 0:
+    print("Division:",num1 / num2)
+else:
+    print("Division: Cannot divide by zero")
---
+++ TASK2.greeting.py
@@ -0,0 +1,6 @@
+first_name = input("Entre your first name:")
+last_name = input ("Entre your last name:")
+
+full_name = first_name + " " + last_name
+
+print("\nHello,",full_name+ "! Welcome to the python program")
